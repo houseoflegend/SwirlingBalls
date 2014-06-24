9d58356994ce6c5c64260cd46b0185a9f54bd091
Larry Legend <larry@houseoflegend.com>
2014-06-24 14:40:34 -0400

    Add fastness property and do code cleanup.

--- FirstSwift/Ball.swift
+++ FirstSwift/Ball.swift
@@ -10,19 +10,27 @@ import Foundation
 import UIKit
 
 class Ball {
-    // Properties
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // MARK: Properties
+
     var radius: CGFloat
     var center: CGPoint = CGPointZero
     var velocityX: CGFloat = 0.0
     var velocityY: CGFloat = 0.0
     var color: UIColor = UIColor.whiteColor()
+    var fastness: CGFloat = 10.0
 
-    // Constructor
+    ////////////////////////////////////////////////////////////////////////////////
+    // MARK: Initialization
+    
     init(aRadius: CGFloat) {
         radius = aRadius
     }
     
-    // Methods
+    ////////////////////////////////////////////////////////////////////////////////
+    // MARK: Instance methods
+    
     func update(tickTime: NSDate, touchPoint: CGPoint, bounds: CGRect) {
         
         center.x = center.x + 1
@@ -39,4 +47,5 @@ class Ball {
         let ellipseRect = CGRect(x:center.x - radius, y: center.y - radius, width: radius * 2.0, height: radius * 2.0)
         CGContextFillEllipseInRect(context, ellipseRect)
     }
+    
 }
